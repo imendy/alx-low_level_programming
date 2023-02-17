d9e05bc49e2bf15c1292be6638f98663e358fc1b
Emmanuel Ndukwe Nmaju <enmaju37@gmail.com>
2023-02-17 08:59:19 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main -  prints all possible different combinations of three digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n, m, i;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (i = 50; i < 58; i++)
+			{
+				if (i > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(i);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
