12f67298f83cf61bf7cfedcf98b9648339701d5f
Emmanuel Ndukwe Nmaju <enmaju37@gmail.com>
2023-02-17 06:48:20 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase except q and e
+ * followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+	putchar('\n');
+	return (0);
+}
