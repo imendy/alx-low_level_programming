f150e2be2cac45e8039e543584ff3e45f096d2c6
Emmanuel Ndukwe Nmaju <enmaju37@gmail.com>
2023-02-21 07:06:36 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,17 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int a;
 
-	if (ld < 0)
-		ld *= -1;
+	if (n < 0)
+		n = -n;
 
-	_putchar(ld + '0');
+	a = n % 10;
 
-	return (0);
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
