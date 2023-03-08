94316ae105d7ec253ff54f5652b36a7d1f6247f2
Emmanuel Ndukwe Nmaju <enmaju37@gmail.com>
2023-03-08 00:53:56 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,18 +1,14 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - Prints a string in reverse
- * @s: The string to be printed
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		_print_rev_recursion(++s);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
