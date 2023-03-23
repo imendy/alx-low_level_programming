c9cd8ca2d74c7cfa74f8224e167d068c10f2aa73
Emmanuel Ndukwe Nmaju <enmaju37@gmail.com>
2023-03-23 07:59:45 -0700

    variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VARIADIC_H_
+#define VARIADIC_H_
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
